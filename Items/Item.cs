a58831cb23c4ca462f4feb0a79ac7736dbccdd61
Haydenwood2005 <28823036@students.lincoln.ac.uk>
2025-03-13 13:03:53 +0000

    Update Item.cs

--- Items/Item.cs
+++ Items/Item.cs
@@ -1,5 +1,8 @@
+
 namespace DungeonExplorer.Item {
-    public abstract class Item {
+
+    
+    public abstract class Item { 
         private static int nextId = 1;
 
         public string Name { get; }
@@ -7,17 +10,14 @@ namespace DungeonExplorer.Item {
         public int Id { get; }
         public bool Useable { get; }
 
-
-        protected Item(string name, string description, bool useable)
+        
+        protected Item(string name, string description, bool useable)  // a new ID of the Item class.
         {
             Name = name;
             Description = description;
             Useable = useable;
             Id = nextId++;
         }
-
-
-        public abstract void Use(Player.Player player);
-        // For public abstract void. Use(Mob.Mob mob);
+        public abstract void Use(Player.Player player); // item is used on the player.
     }
 }
