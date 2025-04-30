ba55ddba1cfc7e50a6abfba63372ba4af489c6ff
Haydenwood2005 <28823036@students.lincoln.ac.uk>
2025-04-30 20:41:59 +0100

    Create Weapons.cs

---
+++ Weapons.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DungeonExplorer
+{
+    internal class Weapons : Items
+    {
+        private static string[] dagger = new string[]
+        {
+            "dagger",
+            "25" // The % of damage added onto attack to hit monster.
+        };
+        
+        private static string[] sword = new string[]
+        {
+            "sword",
+            "50" // The % of damage added onto attack to hit monster.
+        };
+
+        public Weapons() 
+        {
+            Items.Allitems.Add(dagger);
+            Items.Allitems.Add(sword);
+        }
+    }
+}
