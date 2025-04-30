cf381bb54ca500920dd4a62b6b13b8f48325f757
Haydenwood2005 <28823036@students.lincoln.ac.uk>
2025-04-30 20:41:26 +0100

    Create Modifiers.cs

---
+++ Modifiers.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DungeonExplorer
+{
+    internal class WeaponModifiers : Items
+    {
+        private static string[] sharp = new string[]
+        {
+            "sharp",
+            "25" // The % of damage added onto attack to hit monster.
+        };
+
+        private static string[] heavy = new string[]
+        {
+            "heavy",
+            "15" // The % of damage added onto attack to hit monster.
+        };
+
+        public WeaponModifiers()
+        {
+            Items.Allitems.Add(sharp);
+            Items.Allitems.Add(heavy);
+        }
+    }
+}
